724d09bc07706b5e60e3b9c2924ea4a339ba9bb6
Joan Mira <gazpachu@users.noreply.github.com>
2022-08-06 23:11:44 +0900

    fix: removed not used javascript

--- static/js/1-root.js
+++ static/js/1-root.js
@@ -1,56 +1,34 @@
 const themeKey = 'theme';
-const consentKey = 'dataCaptureConsent';
-
-function setConsentModalState(state) {
-  const modalConsent = document.getElementById('modal-consent');
-  if (state) {
-    modalConsent.setAttribute('open', '');
-  } else {
-    modalConsent.removeAttribute('open');
-    localStorage.setItem(consentKey, true);
-  }
-}
 
 document.addEventListener("DOMContentLoaded", function() {
   const html = document.querySelector('html');
   const themeToggleButton = document.querySelector('.theme-toggle');
   const savedTheme = localStorage.getItem(themeKey);
-  const hasConsent = localStorage.getItem(consentKey);
 
   if (savedTheme) {
     html.setAttribute('data-theme', savedTheme);
   } else if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
     html.setAttribute('data-theme', 'dark');
-    if (hasConsent) {
-      localStorage.setItem(themeKey, 'dark');
-    }
+    localStorage.setItem(themeKey, 'dark');
   }
 
   window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
     if (event.matches) {
       html.setAttribute('data-theme', 'dark');
-      if (hasConsent) {
-        localStorage.setItem(themeKey, 'dark');
-      }
+      localStorage.setItem(themeKey, 'dark');
     } else {
       html.setAttribute('data-theme', 'light');
-      if (hasConsent) {
-        localStorage.setItem(themeKey, 'light');
-      }
+      localStorage.setItem(themeKey, 'light');
     }
   });
 
   themeToggleButton.addEventListener('click', event => {
     if (html.getAttribute('data-theme') === 'dark') {
       html.setAttribute('data-theme', 'light');
-      if (hasConsent) {
-        localStorage.setItem(themeKey, 'light');
-      }
+      localStorage.setItem(themeKey, 'light');
     } else {
       html.setAttribute('data-theme', 'dark');
-      if (hasConsent) {
-        localStorage.setItem(themeKey, 'dark');
-      }
+      localStorage.setItem(themeKey, 'dark');
     }
   });
 
@@ -61,14 +39,6 @@ document.addEventListener("DOMContentLoaded", function() {
       item.classList.add('active');
     }
   });
-
-  // Modal consent
-  if (!hasConsent && !window.location.pathname.includes('/privacy')) {
-    setConsentModalState(true);
-  }
-  if (hasConsent && !window.location.host.includes('localhost')) {
-    initDataCapture();
-  }
 
   // Side menu
   const toggleMenuButton = document.getElementById('menu-toggle');
