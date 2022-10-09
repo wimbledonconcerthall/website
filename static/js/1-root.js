cf46dbb347c010153fd63e2293b4ed233170f3e5
Joan Mira <gazpachu@users.noreply.github.com>
2022-10-09 22:21:46 +0900

    feat: added video transcripts to homepage

--- static/js/1-root.js
+++ static/js/1-root.js
@@ -55,4 +55,19 @@ document.addEventListener("DOMContentLoaded", function() {
 
   toggleMenuButton.addEventListener('click', toggleMenu);
   closeMenuButton.addEventListener('click', toggleMenu);
+
+
+  // Collapsibles
+  let myLabels = document.querySelectorAll('.lbl-toggle');
+
+  Array.from(myLabels).forEach(label => {
+    label.addEventListener('keydown', e => {
+      // 32 === spacebar
+      // 13 === enter
+      if (e.which === 32 || e.which === 13) {
+        e.preventDefault();
+        label.click();
+      };
+    });
+  });
 });
